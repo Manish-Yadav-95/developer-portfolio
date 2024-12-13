0687f79daccc09277900d16761dac4ec7a5ea235
ManishKumar Yadav <121492763+Manish-Yadav-95@users.noreply.github.com>
2024-12-13 20:15:21 +0530

    educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,24 +2,18 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
     duration: "2025 - Present",
     institution: "Government Engineering College Rajkot",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
     duration: "2019 - 2021",
     institution: "Tapovan Science School",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
     duration: "2009 - 2019",
     institution: "Shree Gyandeep Shaikshnik School",
   }
