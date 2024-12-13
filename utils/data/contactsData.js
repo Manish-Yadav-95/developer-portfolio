1596cefc31ebab2fdd52dd4ae12301b0d2b5df24
ManishKumar Yadav <121492763+Manish-Yadav-95@users.noreply.github.com>
2024-12-13 20:13:37 +0530

    contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+   email: 'manishyadav3111166@gmail.com',
+    phone: '+919510484151',
+    address: 'Rajkot , Gujarat - 360024',
+    github: 'https://github.com/Manish-Yadav-95',
+    facebook: 'https://www.facebook.com/Manish.Yadav.951',
+    linkedIn: 'https://www.linkedin.com/in/manish-yadav-6ab579252?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    twitter: 'https://x.com/Manish_Yadav_95',
+    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: ""
 }
