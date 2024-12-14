24cf27f7cd1562637e1ea557750b999f2c979227
ManishKumar Yadav <121492763+Manish-Yadav-95@users.noreply.github.com>
2024-12-14 08:30:47 +0530

    personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Manish Yadav",
-  profile: '/profile.jpg',
+  profile: '/passport_photo.jpg',
   designation: "Android Developer",
   description: "My name is Manish Yadav. I am a enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the app more open to the world. My core skill is based on Java and I love to do most of the things using Java. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'manishyadav3111166@gmail.com',
